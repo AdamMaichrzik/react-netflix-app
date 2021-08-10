0993c08784c3cd9f8524d2dc3fad150add0eec0c
Adam Maichrzik <adam.maichrzik@wp.pl>
2021-08-10 12:19:00 +0200

    Navbar logo and 5 spans

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,23 @@
-import "./navbar.scss"
+import "./navbar.scss";
 function Navbar() {
-    return (
-        <div className="navbar">
-            navbar
-        </div>
-    )
+	return (
+		<div className="navbar">
+			<div className="container">
+				<div className="left">
+					<img
+						src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
+						alt="Netflix logo photo"
+					/>
+                    <span>Homepage</span>
+                    <span>Series</span>
+                    <span>Movies</span>
+                    <span>New and popular</span>
+                    <span>My list</span>
+				</div>
+				<div className="right"></div>
+			</div>
+		</div>
+	);
 }
 
-export default Navbar
+export default Navbar;
