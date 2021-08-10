6fae5f23bd6c4486aef16affd21fa9953cff172c
Adam Maichrzik <adam.maichrzik@wp.pl>
2021-08-10 13:03:12 +0200

    Navbar right profile picture and arrow icon

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Notifications, Search } from "@material-ui/icons";
+import { ArrowDropDown, Notifications, Search } from "@material-ui/icons";
 import "./navbar.scss";
 function Navbar() {
 	return (
@@ -9,17 +9,22 @@ function Navbar() {
 						src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
 						alt="Netflix logo photo"
 					/>
-                    <span>Homepage</span>
-                    <span>Series</span>
-                    <span>Movies</span>
-                    <span>New and popular</span>
-                    <span>My list</span>
+					<span>Homepage</span>
+					<span>Series</span>
+					<span>Movies</span>
+					<span>New and popular</span>
+					<span>My list</span>
 				</div>
 				<div className="right">
-                    <Search/>
-                    <span>KID</span>
-                    <Notifications/>
-                </div>
+					<Search />
+					<span>KID</span>
+					<Notifications />
+					<img
+						src="https://avatars.githubusercontent.com/u/64159448?v=4"
+						alt="Profile picture"
+					/>
+                    <ArrowDropDown/>
+				</div>
 			</div>
 		</div>
 	);
