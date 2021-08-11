7cbb3285fdcf0b0faf36cc50cfbe1d2986c2f249
Adam Maichrzik <adam.maichrzik@wp.pl>
2021-08-11 11:11:03 +0200

    Home page photo

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,12 +1,17 @@
-import Navbar from "../components/navbar/Navbar"
-import "./home.scss"
+import Navbar from "../components/navbar/Navbar";
+import "./home.scss";
 
 function Home() {
-    return (
-        <div className="home">
-            <Navbar/>
-        </div>
-    )
+	return (
+		<div className="home">
+			<Navbar />
+			<img
+				width="100%"
+				src="https://geekcat.pl/wp-content/uploads/2021/04/Dr-House.jpeg"
+				alt="Dr House photo"
+			/>
+		</div>
+	);
 }
 
-export default Home
+export default Home;
